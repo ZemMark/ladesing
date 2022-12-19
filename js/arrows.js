6a66b4bb74ee1cb0ce5e6e45b87a0e18a099346d
Mark Zemlianskyi <mark080501@gmail.com>
2022-12-19 10:45:41 +0200

    arrow's animation

---
+++ js/arrows.js
@@ -0,0 +1,40 @@
+const arrow = document.querySelector(".js-arrow");
+const rotateBtn = document.querySelector(".js-rotate");
+// co = document.querySelector('[data-arrow]');
+
+rotateBtn.addEventListener("click", function () {
+  console.log("hello");
+  arrow.classList.toggle("rotated");
+});
+const arrow1 = document.querySelector(".js-arrow1");
+const rotateBtn1 = document.querySelector(".js-rotate1");
+// co = document.querySelector('[data-arrow]');
+
+rotateBtn1.addEventListener("click", function () {
+  console.log("hello");
+  arrow1.classList.toggle("rotated");
+});
+const arrow2 = document.querySelector(".js-arrow2");
+const rotateBtn2 = document.querySelector(".js-rotate2");
+// co = document.querySelector('[data-arrow]');
+
+rotateBtn2.addEventListener("click", function () {
+  console.log("hello");
+  arrow2.classList.toggle("rotated");
+});
+const arrow3 = document.querySelector(".js-arrow3");
+const rotateBtn3 = document.querySelector(".js-rotate3");
+// co = document.querySelector('[data-arrow]');
+
+rotateBtn3.addEventListener("click", function () {
+  console.log("hello");
+  arrow3.classList.toggle("rotated");
+});
+const arrow4 = document.querySelector(".js-arrow4");
+const rotateBtn4 = document.querySelector(".js-rotate4");
+// co = document.querySelector('[data-arrow]');
+
+rotateBtn4.addEventListener("click", function () {
+  console.log("hello");
+  arrow4.classList.toggle("rotated");
+});
